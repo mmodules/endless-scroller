28ee20210fe56807e0b385200f46bf2822bf0bf5
modules <crzammit07@gmail.com>
2026-05-20 01:25:30 +0200

    Add game over screen & retry button

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -1,20 +1,35 @@
 using System;
+using System.Collections;
 using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
     public Camera mainCamera;
     public GameObject explosionObject;
+    public GameObject scoreObject;
     
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Wall"))
         {
+            scoreObject.GetComponent<Score>().timeBetweenIncrements = 200f;
             mainCamera.GetComponent<CameraShake2D>().Shake(1f, 0.3f);
            
             Instantiate(explosionObject, transform.position, transform.rotation);
-            Destroy(gameObject);
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            GetComponent<PlayerMovement>().enabled = false;
+
+            StartCoroutine(Wait());
         }
+    }
+    
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+        Debug.Log("hello");
+        SceneManager.LoadScene("Game Over");
     }
 }
